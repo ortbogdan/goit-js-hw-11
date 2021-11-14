3b97a47611988229693b8ff6c49b76dc911cba7f
Bogdan <ortbg1988@gmail.com>
2021-11-14 14:00:58 +0200

    [start] Started hw-11

--- src/index.js
+++ src/index.js
@@ -1,58 +1,22 @@
-import './css/styles.css';
-import {fetchCountries} from './fetchCountries.js'
-import debounce from 'lodash.debounce'
-import {Notify} from 'notiflix'
+import {fetchGallery} from "./services/api";
+import { Notify } from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
-const listRef = document.querySelector('.country-list');
-const inputRef = document.querySelector('input#search-box');
-const infoRef = document.querySelector('.country-info');
+const formRef = document.querySelector('#search-form');
+const inputRef = document.querySelector('[name="searchQuery"]')
+formRef.addEventListener('submit', searchImages)
+console.dir(formRef)
 
-inputRef.addEventListener('input', debounce(onInputGetCountry, DEBOUNCE_DELAY));
+function searchImages (e) {
+   e.preventDefault()
+   const value = inputRef.value;
+   console.log(value)
+   fetchGallery(value).then(({hits})=>{
+       if (hits.length === 0) {
+           return Notify.info('Sorry, there are no images matching your search query. Please try again.');
+       }
+    console.log(hits)
 
-function onInputGetCountry(){
-    listRef.innerHTML ='';
-    infoRef.innerHTML = '';
-    const value = inputRef.value.trim();
-    if (!value) {
-        return
-    }
-    fetchCountries(value)
-    .then(response => {
-        if(!response.ok) {
-            throw Notify.failure("Oops, there is no country with that name");
-        }
-        return response.json();
-    }
-    ).then(countries =>{
-        if(countries.length > 10) {
-            Notify.info("Too many matches found. Please enter a more specific name.");
-            return
-        }
-        if (countries.length === 1){
-           return makeCountryMarkup(countries)
-        }
-        else{ 
-        return makeCountriesMarkup(countries)}
-        }).catch(console.log)
- }
-
-function makeCountriesMarkup (countries) {
-    
-    listRef.innerHTML = countries.map(({name, flags}) =>`<li class="country-list__item">
-    <img src="${flags.svg}" alt="${name.common} width="30" height="20">
-    <span class="country-list__name">${name.common}<span>
-    </li>`).join('');   
+   })
 }
-function makeCountryMarkup (country) {
-    
-    const {flags, name,  capital,  languages, population} = country[0]
-    const lang = Object.values(languages).join(', ')
-      return infoRef.innerHTML = `<h1><img class="country-info__img" src="${flags.svg}" alt="${name.common} width="30" height="30"">${name.common}<h1>
-        <ul class="country-info__list">
-        <li class="country-info__item">Capital:<span class="country-info__value">${capital}</span></li>
-        <li class="country-info__item">Population:<span class="country-info__value">${population}</span></li>
-        <li class="country-info__item">Languages:<span class="country-info__value">${lang}</span></li>
-        </ul>`
-    }
 
+// {webformatURL, largeImageURL, tags, likes, views, comments, downloads}
---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+
+const URL = 'https://pixabay.com/api/'
+const API_KEY = '24332331-fceed411956b076254def86c5'
+
+function fetchGallery (name) {
+ return fetch (`${URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`).then((response)=>{
+if (!response.ok) {
+    throw console.log("Opps")
+}
+return response.json();
+    })
+}
+export {fetchGallery}
