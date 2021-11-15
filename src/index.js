dee0634cb05fffeb6dcc74e5229fe7a86a69f593
Bogdan <ortbg1988@gmail.com>
2021-11-15 13:47:45 +0200

    [add] Add code

--- src/index.js
+++ src/index.js
@@ -2,21 +2,46 @@ import {fetchGallery} from "./services/api";
 import { Notify } from 'notiflix';
 
 const formRef = document.querySelector('#search-form');
-const inputRef = document.querySelector('[name="searchQuery"]')
+const inputRef = document.querySelector('[name="searchQuery"]');
+const galleryRef = document.querySelector('.gallery');
+
 formRef.addEventListener('submit', searchImages)
 console.dir(formRef)
 
-function searchImages (e) {
+async function searchImages (e) {
    e.preventDefault()
    const value = inputRef.value;
    console.log(value)
-   fetchGallery(value).then(({hits})=>{
-       if (hits.length === 0) {
+   const getImages = await fetchGallery(value)
+   getImages.then((data)=>{
+      
+       if (data.hits.length === 0) {
            return Notify.info('Sorry, there are no images matching your search query. Please try again.');
        }
-    console.log(hits)
-
+       console.log(data.hits)
+    const imagesCards = makeCardMarkup(data.hits);
+    galleryRef.insertAdjacentHTML('beforeend', imagesCards)
    })
 }
-
+// {webformatURL, tags, likes, views, comments, downloads}
 // {webformatURL, largeImageURL, tags, likes, views, comments, downloads}
+function makeCardMarkup (images) {
+   return images.map(({webformatURL, tags, likes, views, comments, downloads}) =>`<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>${views}</b>
+    </p>
+    <p class="info-item">
+      <b>${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>${downloads}</b>
+    </p>
+  </div>
+  </div>`).join('');
+
+}
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 
 const URL = 'https://pixabay.com/api/'
 const API_KEY = '24332331-fceed411956b076254def86c5'
+let page = 1;
 
 function fetchGallery (name) {
  return fetch (`${URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`).then((response)=>{
@@ -11,3 +12,4 @@ return response.json();
     })
 }
 export {fetchGallery}
+// {webformatURL, largeImageURL, tags, likes, views, comments, downloads}
